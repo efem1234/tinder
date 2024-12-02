6dde42fd450fd903b726f39be7d1f01f669d9270
filipmazur <joykey67@gmail.com>
2024-12-02 22:20:00 +0100

    change imports

--- src/components/Navbar/Navbar.component.tsx
+++ src/components/Navbar/Navbar.component.tsx
@@ -1,9 +1,10 @@
 import { Stack, Typography } from "@mui/material";
+import logo from "@assets/logo.png";
 
 export const Navbar: React.FC = () => {
   return (
     <Stack direction="row" alignItems="center" justifyContent="start" gap={2}>
-      <img src="/logo.png" alt="logo" />
+      <img src={logo} alt="logo" />
       <Typography variant="h4">MovieTinder</Typography>
     </Stack>
   );
--- src/store/movies/movies.mock.ts
+++ src/store/movies/movies.mock.ts
@@ -1,23 +1,30 @@
 import { SingleMovieDTO } from "./movies.types";
+import annete from "@assets/moviesPosters/annete.jpg";
+import avengersInfinityWar from "@assets/moviesPosters/avengersInfinityWar.webp";
+import bladeRunner from "@assets/moviesPosters/bladeRunner.webp";
+import chlopi from "@assets/moviesPosters/chlopi.jpeg";
+import kuba from "@assets/moviesPosters/Kuba.webp";
+import starWarsRogueOne from "@assets/moviesPosters/starWarsRogueOne.jpg";
+import tedLasso from "@assets/moviesPosters/tedLasso.jpg";
 
 export const moviesMockedData: SingleMovieDTO[] = [
   {
     id: "1",
-    imageURL: "/moviesPosters/annete.jpg",
+    imageURL: annete,
     title: "Annete",
     summary: `"Annette" is a 2021 musical drama by Leos Carax, following the turbulent relationship between Henry, a stand-up comedian, and Ann, a famous opera singer. They have a daughter, Annette, who possesses an extraordinary and mysterious talent. As Henry's career falters and Ann's rises, their relationship becomes strained, leading to emotional and psychological conflicts. The film explores themes of fame, obsession, and the impact of celebrity on family life. With music by Sparks, "Annette" features haunting visuals and intense performances by Adam Driver and Marion Cotillard. The film’s dark narrative and surreal style create a unique and thought-provoking cinematic experience.`,
     rating: 4.5,
   },
   {
     id: "2",
-    imageURL: "/moviesPosters/avengersInfinityWar.webp",
+    imageURL: avengersInfinityWar,
     title: "Avengers: Infinity War",
     summary: `Avengers: Infinity War (2018) is a superhero film directed by Anthony and Joe Russo, part of the Marvel Cinematic Universe. The movie centers around the villain Thanos, who seeks to collect all six Infinity Stones to achieve ultimate power and reshape the universe. The Avengers, along with the Guardians of the Galaxy and other allies, unite to stop him. As Thanos' power grows, the heroes struggle to protect the Stones and prevent the inevitable destruction he plans. The film explores themes of sacrifice, heroism, and the weight of failure. The action-packed storyline builds towards a devastating climax, where Thanos succeeds in using the Stones to erase half of all life in the universe. The film is known for its high-stakes plot, emotional depth, and the surprising loss of several beloved characters. The ensemble cast includes Robert Downey Jr., Chris Hemsworth, Scarlett Johansson, and many others. Infinity War is both a thrilling spectacle and a poignant exploration of loss. The film ends on a cliffhanger, leading directly into Avengers: Endgame.`,
     rating: 3.5,
   },
   {
     id: "3",
-    imageURL: "/moviesPosters/bladeRunner.webp",
+    imageURL: bladeRunner,
     title: "Blade Runner 2049",
     summary:
       "Blade Runner 2049 (2017), directed by Denis Villeneuve, is a sequel to the 1982 classic Blade Runner. Set 30 years after the original, the film follows K (Ryan Gosling), a new blade runner who uncovers a long-buried secret that could reshape society. His discovery leads him on a quest to find Rick Deckard (Harrison Ford), a former blade runner who has been missing for decades. The film explores themes of identity, memory, and the nature of humanity, questioning what it means to be real. Known for its stunning cinematography, visual effects, and atmospheric tone, Blade Runner 2049 expands on the original’s philosophical exploration of artificial life. The film’s slow-paced, contemplative narrative is complemented by a haunting score by Hans Zimmer and Benjamin Wallfisch. It received critical acclaim for its depth, world-building, and performances.",
@@ -25,7 +32,7 @@ export const moviesMockedData: SingleMovieDTO[] = [
   },
   {
     id: "4",
-    imageURL: "/moviesPosters/chlopi.jpeg",
+    imageURL: chlopi,
     title: "Chłopi",
     summary:
       "The 2024 film Chłopi (The Peasants) is an animated adaptation of Władysław Reymont's Nobel Prize-winning novel. Directed by DK Welchman and Hugh Welchman, it tells the story of Jagna, a young woman in a 19th-century Polish village. Caught in conflicts of love and societal expectations, her defiance of patriarchal norms leads to a tragic clash with her community. The film is notable for its unique painted animation style and vivid depiction of rural life. It premiered at TIFF 2023 and is Poland's entry for the 96th Academy Awards",
@@ -33,21 +40,21 @@ export const moviesMockedData: SingleMovieDTO[] = [
   },
   {
     id: "5",
-    imageURL: "/moviesPosters/Kuba.webp",
+    imageURL: kuba,
     title: "Kuba ",
     summary: `The 2024 film Kuba is a documentary directed by Jan Dybus, focusing on the life and career of Jakub "Kuba" Błaszczykowski, a celebrated Polish footballer. The film chronicles his journey from humble beginnings to becoming a star winger for Borussia Dortmund and the Polish national team. It highlights his resilience, leadership, and ability to inspire both on and off the field, even in the face of personal and professional challenges. Featuring appearances by notable figures like Jürgen Klopp and Robert Lewandowski, the film offers insights into Błaszczykowski’s impact on football and his enduring legacy in the sport. It is a deeply personal story that resonates with fans of sports and perseverance alike, premiering on February 23, 2024`,
     rating: 3.3,
   },
   {
     id: "6",
-    imageURL: "/moviesPosters/starWarsRogueOne.jpg",
+    imageURL: starWarsRogueOne,
     title: "Star Wars: Rogue One",
     summary: `Star Wars: Rogue One (2016), directed by Gareth Edwards, is a standalone prequel to A New Hope. It tells the story of Jyn Erso (Felicity Jones) and a band of Rebels who embark on a dangerous mission to steal the Death Star plans, a crucial step in the fight against the Galactic Empire. With characters like Cassian Andor (Diego Luna) and the droid K-2SO (Alan Tudyk), the film captures the cost of rebellion, focusing on sacrifice and hope in the face of overwhelming odds. Notable for its darker tone and stunning visuals, the movie features unforgettable moments, such as the Battle of Scarif and Darth Vader's chilling hallway attack. It seamlessly ties into the beginning of A New Hope, enriching the saga's lore and emphasizing the importance of unity against tyranny. Rogue One was widely praised for its action, emotional depth, and commitment to the Star Wars universe.`,
     rating: 2.3,
   },
   {
     id: "7",
-    imageURL: "/moviesPosters/tedLasso.jpg",
+    imageURL: tedLasso,
     title: "Ted Lasso",
     summary:
       "Ted Lasso is a popular comedy-drama television series developed by Jason Sudeikis, Bill Lawrence, Brendan Hunt, and Joe Kelly. It premiered on Apple TV+ in 2020 and quickly became a cultural phenomenon. The series centers on Ted Lasso (Jason Sudeikis), an American college football coach hired to manage AFC Richmond, a struggling English Premier League soccer team, despite having no experience in the sport",
